6ff8f46e58150499235e296eda761b49e231441f
Bartosz <gazdrinker2@gmail.com>
2024-07-17 20:14:48 +0200

    code optimization

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,8 @@
-import Card from "./Card";
+import Card from './Card';
 
 export default function Results({ results }) {
 	return (
-		<div className="sm:grid sm:grid-cols-3 lg:grid-cols-4 max-w-7xl mx-auto p-8 md:p-4 gap-10">
+		<div className='sm:grid sm:grid-cols-3 lg:grid-cols-4 max-w-7xl mx-auto p-8 md:p-4 gap-10'>
 			{results.map((result) => (
 				<Card key={result.id} result={result} />
 			))}
