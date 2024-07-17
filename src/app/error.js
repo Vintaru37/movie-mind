c95457acb4a2b0425a46abead87e7d00f3a87911
Bartosz <gazdrinker2@gmail.com>
2024-07-17 15:33:10 +0200

    handle fetching error

---
+++ src/app/error.js
@@ -0,0 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+export default function error({ error, reset }) {
+	useEffect(() => {
+		console.log(error);
+	}, [error]);
+	return (
+		<div className='text-center mt-10'>
+			<h1>Something went wrong. Please try again later.</h1>
+            <button className='mt-4 py-2 px-4 border rounded-md border-gray-700 dark:border-gray-300 hover:text-amber-400 dark:hover:border-amber-400 dark:hover:bg-slate-700 duration-150' onClick={reset}>Try again</button>
+		</div>
+	);
+}
